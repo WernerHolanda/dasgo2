8e69929bbfb5cf204484dc971ecd4634d8e3bfb9
Werner <dvaice.werner@gmail.com>
2023-08-04 12:36:58 -0300

    iv-01-02-04

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
 import React from 'react'
+import { RiSearchLine, RiUserAddLine, RiNotificationLine } from 'react-icons/ri'
 
 export function Header(){
     return(
@@ -8,13 +9,79 @@ export function Header(){
         w="100%" 
         maxWidth={1480}
         h="20"
-        mx="auto"
-        mt="4"
-        px="6"
+        mx="auto" //=marginhorizontal
+        mt="4" //=margintop
+        px="6" //=padding horizontal
         align="center"
         >
-            <h1>DASHGO</h1>
-        </Flex>
+        <Text 
+                fontSize="3x1"
+                fontWeight="bold"
+                letterSpacing="tight"
+                w="64"
+                >
+                dashgo
+                <Text as="span" ml="1" color="pink.500">.</Text>
+        </Text>
+        
+        <Flex 
+            as="label"
+            flex="1"
+            py="4" //paddingvertical
+            px="8"//padding horizontal
+            ml="6"
+            maxWidth={400}
+            alignSelf="center"//se alinhar ao centro
+            color="gray.200"
+            position="relative"
+            bg="gray.800"
+            borderRadius-="full"
+        >
+        <Input
+            color="gray.50"
+            variant="unstyled" // é para não ter a formatação original
+            px="4"
+            mr="4" //marginright
+            placeholder="Buscar na plataforma"
+            _placeholder={{ color: 'gray.400'}}
+        />
+        <Flex 
+            align="center"
+            ml="auto"
+        >
+            <HStack
+                spacing="8"
+                mx="8"
+                pr="8" //poadding right
+                py="1" //padding vertical
+                color="gray.300"
+                borderRightWidth={1}
+                borderColor="gray.700"
+            >
+                <Icon as={RiSearchLine} //isso é uma instrução do chakra lib
+                    fontSize="20"
+                /> 
 
-    )
+                <Icon as={RiUserAddLine} //isso é uma instrução do chakra lib
+                    fontSize="20"
+                /> 
+            </HStack>
+
+            <Flex align="center">
+                <Box mr="4" textAlign="right">
+                    <Text>Werner Holanda</Text>
+                    <Text color="gray.300" fontSize="small">
+                        werner.holanda.dev@gmail.com
+                    </Text>
+                </Box>
+
+                <Avatar size="md" name="Werner Holanda" src="https://github.com/WernerHolanda.png" />
+            
+
+            </Flex>
+        </Flex>
+        </Flex>
+         
+        </Flex>
+    );
 }
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,3 +1,4 @@
+import { Header } from "@/components/Header";
 import React from "react";
 
 export default function Dashboard(){
