ccf7f7c349ea4d87cb22fdd0e7ee977b75c4b02a
Werner <dvaice.werner@gmail.com>
2023-08-07 18:11:39 -0300

    cpt1-01-03-2

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,15 @@
+import { Button, Stack } from "@chakra-ui/react";
+
+
+export function Pagination() {
+    return (
+    <Stack>
+        <Button size="sm"
+        fontSize="xs"
+        width="4"
+
+        >
+        </Button>
+    </Stack>
+    );
+}
