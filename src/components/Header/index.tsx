5baa98f789bf98c7b54567ade469b46e8ce6f077
Werner <dvaice.werner@gmail.com>
2023-08-08 22:40:15 -0300

    iv-1-4-4

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,19 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react'
 import React from 'react'
 import { Profile } from './Profile';
 import { NotificationsNav } from './NotificationsNav';
 import { SearchBox } from './SearchBox';
 import { Logo } from './Logo';
+import { useSidebarDrawer } from '../contexts/SidebarDrawerContext';
+import { RiMenuLine } from 'react-icons/ri';
 
 export function Header(){
+    const {onOpen} = useSidebarDrawer()
+
+    const isWideVersion = useBreakpointValue({
+        base: false, //por padrão é falso, logo não é visivel os dados e nomes do perfil da pessoa na pagina. Isso é uma solução para que quando estiver no dispositivo mobile as informações não fiquem muito apertadas. Basta apenas a imagem da logo da pessoa, não precisa de todos os dados. 
+        lg:true, // mas caso não seja o padrão então ele ficará large=lg ai então ele mostrará os dados do lado da logo da pessoa. 
+    })
     return(
         <Flex 
         as="header" 
@@ -17,14 +25,25 @@ export function Header(){
         px="6" //=padding horizontal
         align="center"
         >
+                {!isWideVersion && (
+                    <IconButton
+                    aria-label="open Navigation" //é obrigatório pra dizer o que q ela faz
+                    icon={<Icon as={RiMenuLine} />}
+                    fontSize="24"
+                    variant="unstyled"
+                    onClick={onOpen}
+                    mr="2" 
+                    >
+
+                    </IconButton>)}
             <Logo />
         
-            <SearchBox />
+            { isWideVersion && <SearchBox /> }
 
         <Flex align="center" ml="auto">    
             <NotificationsNav />
 
-            <Profile />
+            <Profile showProfileData={isWideVersion} />
         </Flex>
         
          
